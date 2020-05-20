30ff901acbba3ad5bc6363a071dddece9e5658e1
Nick Luchian <nicolae.luchian@endava.com>
2020-05-20 15:12:20 +0300

    Initial commit

---
+++ src/com/company/Video.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Video {
+    String videoGenre;
+    long year;
+
+    public Video(String videoGenre, long year) {
+        this.videoGenre = videoGenre;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" +
+                "videoGenre='" + videoGenre + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
